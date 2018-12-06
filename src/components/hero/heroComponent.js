d531e55d7c2421f9dee0ff13faf1c6ea0a6a4ee5
Daniela Odileth Osorto Padilla <dosorto@loe.corp>
2018-12-06 11:45:45 -0600

    change subtitle

--- src/components/hero/heroComponent.js
+++ src/components/hero/heroComponent.js
@@ -7,7 +7,7 @@ class HeroComponent extends Component {
       <section className="main-section hero-header section--with-backdrop section--with-parallax">
         <h2 className="hero-header__subtitle">Hello there! I am</h2>
         <h1 className="hero-header__title">Daniela Osorto</h1>
-        <h2 className="hero-header__subtitle">Jr Web developer, HN</h2>
+        <h2 className="hero-header__subtitle">Web Developer, HN</h2>
     </section>
     );
   }
